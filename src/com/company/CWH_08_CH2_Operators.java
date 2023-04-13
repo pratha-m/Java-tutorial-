452f46cab2f1e4299aa75acc2b1c0cd99965e819
pratha-m <prathamchhabra85@gmail.com>
2023-04-13 11:08:46 +0530

    commit

--- src/com/company/CWH_08_CH2_Operators.java
+++ src/com/company/CWH_08_CH2_Operators.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class CWH_08_CH2_Operators {
+ public static void main(String[] args){
+//    arithmetic
+//    int a1=7;
+//    int a2=5;
+//    System.out.println("5+6 :"+(5+6));
+//    System.out.println("5-6 :"+(5-6));
+//    System.out.println("5*6 :"+(5*6));
+//    System.out.println("5/6 :"+(5/6));
+//    System.out.println("5%6 :"+(5%6));
+//    System.out.println("a1++ :"+(a1++));
+//    System.out.println("a1 :"+a1);
+//    System.out.println("a2-- :"+(a2--));
+//    System.out.println("a2 :"+a2);
+//    System.out.println("5.2%2.2 :"+(5.2%2.2));
+
+//    Assignment
+//      int b1=8;
+//      int b2=5;
+//      b1+=3;
+//      System.out.println("b1 :"+b1);
+//      b2-=2;
+//      System.out.println("b2 :"+b2);
+
+//    Comparison
+//      System.out.println("5>8 :"+(5>8));
+//      System.out.println("5<8 :"+(5<8));
+//      System.out.println("5==8 :"+(5==8));
+//      System.out.println("5==5 :"+(5==5));
+//      System.out.println("5>=15 :"+(5>=15));
+//      System.out.println("5<=15 :"+(5<=15));
+
+//     logical operators
+//    System.out.println((5>2) && (5>3));
+//    System.out.println((5>2) && (5<3));
+//    System.out.println((5>2) || (5<3));
+//    System.out.println(!(5>3));
+//    System.out.println(!(5<3));
+
+//     bitwise opertors
+//    System.out.println("2&3 :"+(2&3));  //2-->10,3-->11  10 & 11 -->10 that is 2
+//    System.out.println("2|3 :"+(2|3));  //2-->10,3-->11  10 | 11 -->11 that is 3
+ }
 }
---
+++ src/com/company/CWH_09_CH2_OPERA_PRECEDENCE.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class CWH_09_CH2_OPERA_PRECEDENCE {
+    public static void main(String[] args) {
+//          int a=6*5-34/2; // * and / have same precendence but associativity left to right so evaluation from left to right
+          /*
+            =30-34/2
+            =30-17
+            =13
+           */
+//          int b=60/5-34*2; // / and * have same precendence but associativity left to right so evaluation from left to right
+          /*
+            =12-34*2
+            =12-68
+            =-56
+           */
+
+//        Quick Quiz
+          int x=2,y=6;
+          int exp1=(x-y)/2;
+          int a=4,b=5,c=9,d=8;
+          int exp2=b*b-(4*a*c)/(2*a);
+          int v=2,u=1;
+          int exp3=v*v-u*u;
+          int exp4=a*b-d;
+          System.out.println(exp1);
+          System.out.println(exp2);
+          System.out.println(exp3);
+          System.out.println(exp4);
+    }
+}
