97d8c82dcdc46490e0a25c06506e494b81070be2
pratha-m <prathamchhabra85@gmail.com>
2024-03-26 00:08:30 +0530

    starting insertion in DLL

--- src/com/company/DSA TOPICS/Linked List/doublyLinkedList/DoublyLinkedList.java
+++ src/com/company/DSA TOPICS/Linked List/doublyLinkedList/DoublyLinkedList.java
@@ -44,21 +44,29 @@ public class DoublyLinkedList {
     public static Node deleteKthNode(Node head,int k){
         if(head==null || head.next==null) return null;
         Node temp=head;
-        int i=1;
-        while(i!=k){
-            temp=temp.next;
+        int i=0;
+        System.out.println("P1");
+        while(temp!=null){
             i++;
+            if(i==k) break;
+            temp=temp.next;
         }
+        System.out.println("P2");
         Node prev=temp.back;
         Node forward=temp.next;
+        System.out.println("P3");
         if(prev==null && forward==null) return null;
         else if(prev==null) return deleteHeadNode(head);
         else if(forward==null) return deleteTailNode(head);
         prev.next=forward;
         forward.back=prev;
+        System.out.println("P3");
         temp.next=null;
         temp.back=null;
         return head; 
+    }
+    public static Node insertAtStart(Node head,int data){
+
     }
     public static void printLinkedlist(Node head){
         while(head!=null){
